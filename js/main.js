bc30cec81ebfb3b0225bb16577b5281324ba9579
Leon <144380693+Leonidas1987@users.noreply.github.com>
2023-09-28 00:46:22 +0300

    Lesson_12
    Finish!

---
+++ js/main.js
@@ -0,0 +1,26 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// ========Infinity=========
+// console.log(-4/0);
+
+// ========Not a number=====
+// console.log("String" * 9);
+
+// ========Boolean==========
+// const bool = false;
+
+// ========Undefined============
+// let und;
+// console.log(und);
+
+// ========Object============
+const obj = {
+  name: "Leon",
+  age: 36,
+  isMarried: true
+};
+console.log(obj);
+console.log(obj.age);
+console.log(obj.isMarried);
